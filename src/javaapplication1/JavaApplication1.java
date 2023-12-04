28170d46f8f1473a2b2c32d868000b6f5af0ab6c
Taruc <Taruc@B100K-PC9>
2023-12-04 10:35:01 +0800

    hihih

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,10 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");
+        System.out.println("hello world");
+        System.out.println("hello");
+         System.out.println("gugugaga");
+        
     }
     
 }
